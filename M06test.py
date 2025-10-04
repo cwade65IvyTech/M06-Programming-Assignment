76695f4193d57ea27b548a9c44fa6cabe90c60ae
cwade65IvyTech <157136592+cwade65IvyTech@users.noreply.github.com>
2025-10-04 08:57:02 -0400

    Add files via upload

--- M06test.py
+++ M06test.py
@@ -7,5 +7,10 @@ def kinda_times():
     print(datetime.strftime(datetime.today(), "%H:%M:%S:%f"))
 if __name__ == '__main__':
     for x in range(3):
+        processes = []
         p = Process(target=kinda_times)
         p.start()
+        processes.append(p)
+        for p in processes:
+            p.join()
+    print("Processes completed.")
